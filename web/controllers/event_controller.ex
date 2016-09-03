422b1c536b5a520d5cfcf126484dd1f81b1c7352
Arjan van der Gaag <arjan@arjanvandergaag.nl>
2016-09-03 11:06:25 +0200

    Rewrite nested `case` statement using `with`

--- web/controllers/event_controller.ex
+++ web/controllers/event_controller.ex
@@ -4,26 +4,19 @@ defmodule Wall.EventController do
   alias Wall.Event
 
   def create(conn, event_params) do
-    case Base.decode64(event_params["token"]) do
-      {:ok, token} ->
-        case Phoenix.Token.verify(Wall.Endpoint, "token", token) do
-          {:ok, project_id} ->
-            changeset = Event.changeset(%Event{}, Map.put(event_params, "project_id", project_id))
-            case Repo.insert(changeset) do
-              {:ok, event} ->
-                conn
-                |> put_status(:created)
-                |> render("show.json", event: event)
-              {:error, changeset} ->
-                conn
-                |> put_status(:unprocessable_entity)
-                |> render(Wall.ErrorView, "errors.json", changeset: changeset)
-            end
-          {:error, :invalid} ->
-            conn
-            |> put_status(:unprocessable_entity)
-            |> json(%{error: "invalid token"})
-        end
+    with {:ok, token} <-  Base.decode64(event_params["token"]),
+         {:ok, project_id} <- Phoenix.Token.verify(Wall.Endpoint, "token", token),
+         changeset = Event.changeset(%Event{}, Map.put(event_params, "project_id", project_id)),
+         {:ok, event} <- Repo.insert(changeset)
+    do
+      conn
+      |> put_status(:created)
+      |> render("show.json", event: event)
+    else
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(Wall.ErrorView, "errors.json", changeset: changeset)
       _ ->
         conn
         |> put_status(:unprocessable_entity)
